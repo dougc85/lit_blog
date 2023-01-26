6437fbec3d078a2ac2957e33a4117382ae6bc0db
Doug Carter <dougc85@gmail.com>
2023-01-25 16:30:52 -0800

    implemented deletePost

--- src/components/PostOptions/PostOptions.js
+++ src/components/PostOptions/PostOptions.js
@@ -1,6 +1,8 @@
 import React, { useContext } from 'react';
 import './PostOptions.css';
 
+import { useNavigate } from 'react-router-dom';
+
 import AuthContext from '../../context/auth-context';
 
 function PostOptions(props) {
@@ -19,6 +21,8 @@ function PostOptions(props) {
     published,
     _id
   } = post;
+
+  const navigate = useNavigate();
 
   const {
     authObject
@@ -71,6 +75,33 @@ function PostOptions(props) {
 
   function handleDelete(e) {
     e.preventDefault();
+
+    const headers = {
+      'Content-Type': 'application/json',
+    };
+
+    if (authObject) {
+      headers.Authorization = 'Bearer ' + authObject.token
+    }
+
+    fetch(process.env.REACT_APP_URL + `/posts/${_id}`, {
+      method: 'DELETE',
+      headers
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(result => {
+        if (result.error) {
+          throw new Error(result.message);
+        } else {
+          console.log(result, 'result');
+          navigate('/');
+        }
+      })
+      .catch(err => {
+        console.log(err, 'error');
+      })
   }
 
   return (
